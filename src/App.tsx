5911ffac43d726ccaf5f2b9b2cffaef300830790
Haziq Musa <last.game01987@gmail.com>
2022-04-22 07:48:12 +0800

    Working on SearchItem component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { logOut, auth, emailAndPasswordSignIn, db } from "./firebase";
 import { onAuthStateChanged } from "firebase/auth";
-import { addDoc, collection, getDocs } from "firebase/firestore";
+import { addDoc, collection, getDocs, query, where } from "firebase/firestore";
 
 function App() {
   const [user, setUser] = useState<string | null>(null);
@@ -19,7 +19,9 @@ function App() {
   return (
     <>
       <h1>Hi UniPS</h1>
-      {user ? <MainContent /> : <SignIn />}
+      {user && <SignOut />}
+      {user ? <AddParcelRecord /> : <SignIn />}
+      <SearchItem />
     </>
   );
 }
@@ -49,7 +51,7 @@ const SignIn = () => {
         onChange={e => setPassword(e.target.value)}
         className="password"
       />
-      <button>Login</button>
+      <button className="btn-primary">Login</button>
     </form>
   );
 };
@@ -64,23 +66,41 @@ const SignOut = () => {
   );
 };
 
-const MainContent = () => {
-  const addToDoc = async () => {
+const AddParcelRecord = () => {
+  const [success, setSuccess] = useState(false);
+  const [trackNum, setTrackNum] = useState("");
+  const [date, setDate] = useState("");
+
+  const onSuccess = () => {
+    setSuccess(true);
+
+    setTimeout(() => setSuccess(false), 2000); // after 2 secs it set to false
+  };
+
+  const clearForm = () => {
+    setTrackNum("");
+    setDate("");
+  };
+
+  const addToDoc = async (e: any) => {
+    e.preventDefault();
+
     try {
-      const docRef = await addDoc(collection(db, "users"), {
-        first: "Alan",
-        middle: "Mathison",
-        last: "Turing",
-        born: 1912,
+      const docRef = await addDoc(collection(db, "items"), {
+        trackNum: trackNum,
+        dateReceive: date,
+        received: false,
       });
-      console.log("Document written with ID: ", docRef.id);
+
+      onSuccess();
+      clearForm();
     } catch (e) {
       console.error("Error adding document: ", e);
     }
   };
 
   const readData = async () => {
-    const querySnapshot = await getDocs(collection(db, "users"));
+    const querySnapshot = await getDocs(collection(db, "messages"));
     querySnapshot.forEach(doc => {
       console.log(`${doc.id} => ${doc.data()}`);
     });
@@ -88,11 +108,95 @@ const MainContent = () => {
 
   return (
     <>
-      <div>
-        <button onClick={addToDoc}>Add to doc</button>
-        <button onClick={readData}>Read data</button>
-      </div>
-      <SignOut />
+      {success && <SuccessMessage />}
+      <form onSubmit={addToDoc}>
+        <div>
+          <label htmlFor="trackNum">Tracking Number</label>
+          <input
+            id="trackNum"
+            type="text"
+            onChange={e => setTrackNum(e.target.value)}
+            value={trackNum}
+          />
+        </div>
+        <div>
+          <label htmlFor="date">Date of receive</label>
+          <input
+            id="date"
+            type="date"
+            onChange={e => setDate(e.target.value)}
+            value={date}
+          />
+        </div>
+        <button className="btn-primary">Add item</button>
+      </form>
     </>
+  );
+};
+
+const SuccessMessage = () => {
+  return (
+    <>
+      <p>The item is successfully added to record!</p>
+    </>
+  );
+};
+
+const SearchItem = () => {
+  const [search, setSearch] = useState("");
+  const [result, setResult] = useState("");
+
+  const readItem = async () => {
+    const itemsRef = collection(db, "items");
+    const q = query(itemsRef, where("trackNum", "==", search));
+    const itemsSnap = await getDocs(q);
+
+    // execute query
+    if (itemsSnap.docs.length > 0) {
+      itemsSnap.forEach(doc => {
+        const data = doc.data();
+        setResult(JSON.stringify(data));
+      });
+    } else {
+      setResult(JSON.stringify({ error: `${search} not found` }));
+    }
+  };
+
+  const onSearch = (e: any) => {
+    e.preventDefault();
+    readItem();
+  };
+
+  return (
+    <>
+      <form onSubmit={onSearch}>
+        <input
+          type="text"
+          onChange={e => setSearch(e.target.value)}
+          placeholder="Search tracking number"
+          value={search}
+        />
+        <button>Search</button>
+      </form>
+
+      {result && <SearchResult result={result} />}
+    </>
+  );
+};
+
+const SearchResult = ({ result }: any) => {
+  const data = result && JSON.parse(result);
+
+  return (
+    <div>
+      {data.error ? (
+        <p>{data.error}</p>
+      ) : (
+        <>
+          <p>Tracking no: {data.trackNum}</p>
+          <p>Receive: {data.dateReceive}</p>
+        </>
+      )}
+    </div>
   );
 };
